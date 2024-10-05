c78301596396d00e0e1c388e563f9ba741b0a900
ZeroN2 <42255304+ZeroN2@users.noreply.github.com>
2024-10-05 17:21:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ client.on('ready', () => {
 
 		function generateRandomString(length) {
 			let result = '';
-			const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+			const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 			const charactersLength = characters.length;
 			for (let i = 0; i < length; i++) {
 				result += characters.charAt(Math.floor(Math.random() * charactersLength));
